557d8832dd9689cd717661aaf90f0c9698afdfa1
mcampi111 <51228636+mcampi111@users.noreply.github.com>
2020-08-26 11:39:48 +0100

    Code for SVM with Laplace Kernel

---
+++ Code/OutOfSample_Code/Laplace_Kernel/SVM_SYNT_SPEAK1_Results_IF_out.R
@@ -0,0 +1,99 @@
+#Tabulating results using kable function
+library('knitr')
+library('kableExtra')
+library('tcpl')
+
+
+#SVM SYNTHETIC vs SPEAKER 1 - IF
+
+#ACCURACY
+
+acc_IF1_SYNTSPEAK1<- as.numeric(CM_syntspeak1_IF1_new_lp$overall[1])
+acc_IF2_SYNTSPEAK1<- as.numeric(CM_syntspeak1_IF2_new_lp$overall[1])
+acc_IF3_SYNTSPEAK1<- as.numeric(CM_syntspeak1_IF3_new_lp$overall[1])
+acc_IF4_SYNTSPEAK1<- as.numeric(CM_syntspeak1_IF4_new_lp$overall[1])
+acc_IF5_SYNTSPEAK1<- as.numeric(CM_syntspeak1_IF5_new_lp$overall[1])
+
+acc_IF_SYNTSPEAK1_lp<- c(acc_IF1_SYNTSPEAK1,acc_IF2_SYNTSPEAK1,acc_IF3_SYNTSPEAK1, acc_IF4_SYNTSPEAK1, acc_IF5_SYNTSPEAK1)
+
+acc_IF_SYNTSPEAK1<- c(acc_IF1_SYNTSPEAK1,acc_IF2_SYNTSPEAK1,acc_IF3_SYNTSPEAK1, acc_IF4_SYNTSPEAK1, acc_IF5_SYNTSPEAK1)
+
+#F1 score
+
+F1_IF1_SYNTSPEAK1<- as.numeric(CM_syntspeak1_IF1_new_lp$byClass[7])
+F1_IF2_SYNTSPEAK1<- as.numeric(CM_syntspeak1_IF2_new_lp$byClass[7])
+F1_IF3_SYNTSPEAK1<- as.numeric(CM_syntspeak1_IF3_new_lp$byClass[7])
+F1_IF4_SYNTSPEAK1<- as.numeric(CM_syntspeak1_IF4_new_lp$byClass[7])
+F1_IF5_SYNTSPEAK1<- as.numeric(CM_syntspeak1_IF5_new_lp$byClass[7])
+
+
+F1_IF_SYNTSPEAK1<- c(F1_IF1_SYNTSPEAK1,F1_IF2_SYNTSPEAK1,F1_IF3_SYNTSPEAK1, F1_IF4_SYNTSPEAK1, F1_IF5_SYNTSPEAK1)
+
+#Kappa
+
+# Kappa_IF1_SYNTSPEAK1<- as.numeric(CM_syntspeak1_IF1_new_lp$overall[2])
+# Kappa_IF2_SYNTSPEAK1<- as.numeric(CM_syntspeak1_IF2_new_lp$overall[2])
+# Kappa_IF3_SYNTSPEAK1<- as.numeric(CM_syntspeak1_IF3_new_lp$overall[2])
+# Kappa_IF4_SYNTSPEAK1<- as.numeric(CM_syntspeak1_IF4_new_lp$overall[2])
+# Kappa_IF5_SYNTSPEAK1<- as.numeric(CM_syntspeak1_IF5_new_lp$overall[2])
+# 
+# 
+# Kappa_IF_SYNTSPEAK1<- c(Kappa_IF1_SYNTSPEAK1,Kappa_IF2_SYNTSPEAK1,Kappa_IF3_SYNTSPEAK1, Kappa_IF4_SYNTSPEAK1, 
+#                      Kappa_IF5_SYNTSPEAK1)
+
+#Precision
+
+Precision_IF1_SYNTSPEAK1<- as.numeric(CM_syntspeak1_IF1_new_lp$byClass[5])
+Precision_IF2_SYNTSPEAK1<- as.numeric(CM_syntspeak1_IF2_new_lp$byClass[5])
+Precision_IF3_SYNTSPEAK1<- as.numeric(CM_syntspeak1_IF3_new_lp$byClass[5])
+Precision_IF4_SYNTSPEAK1<- as.numeric(CM_syntspeak1_IF4_new_lp$byClass[5])
+Precision_IF5_SYNTSPEAK1<- as.numeric(CM_syntspeak1_IF5_new_lp$byClass[5])
+
+Precision_IF_SYNTSPEAK1<- c(Precision_IF1_SYNTSPEAK1,Precision_IF2_SYNTSPEAK1,Precision_IF3_SYNTSPEAK1,
+                            Precision_IF4_SYNTSPEAK1, Precision_IF5_SYNTSPEAK1)
+
+#Sensitivity
+
+Sens_IF1_SYNTSPEAK1<- as.numeric(CM_syntspeak1_IF1_new_lp$byClass[1])
+Sens_IF2_SYNTSPEAK1<- as.numeric(CM_syntspeak1_IF2_new_lp$byClass[1])
+Sens_IF3_SYNTSPEAK1<- as.numeric(CM_syntspeak1_IF3_new_lp$byClass[1])
+Sens_IF4_SYNTSPEAK1<- as.numeric(CM_syntspeak1_IF4_new_lp$byClass[1])
+Sens_IF5_SYNTSPEAK1<- as.numeric(CM_syntspeak1_IF5_new_lp$byClass[1])
+
+Sens_IF_SYNTSPEAK1<- c(Sens_IF1_SYNTSPEAK1,Sens_IF2_SYNTSPEAK1,Sens_IF3_SYNTSPEAK1, Sens_IF4_SYNTSPEAK1,
+                       Sens_IF5_SYNTSPEAK1)
+
+#Specificity 
+
+Spec_IF1_SYNTSPEAK1<- as.numeric(CM_syntspeak1_IF1_new_lp$byClass[2])
+Spec_IF2_SYNTSPEAK1<- as.numeric(CM_syntspeak1_IF2_new_lp$byClass[2])
+Spec_IF3_SYNTSPEAK1<- as.numeric(CM_syntspeak1_IF3_new_lp$byClass[2])
+Spec_IF4_SYNTSPEAK1<- as.numeric(CM_syntspeak1_IF4_new_lp$byClass[2])
+Spec_IF5_SYNTSPEAK1<- as.numeric(CM_syntspeak1_IF5_new_lp$byClass[2])
+
+Spec_IF_SYNTSPEAK1<- c(Spec_IF1_SYNTSPEAK1,Spec_IF2_SYNTSPEAK1,Spec_IF3_SYNTSPEAK1, Spec_IF4_SYNTSPEAK1,
+                       Spec_IF5_SYNTSPEAK1)
+
+#PRINT RESULTS ON A TABLE
+nsvm<- c('IF1', 'IF2', 'IF3', 'IF4','IF5')
+
+cnames_1<- list('SYNT vs SPEAK1 ', 'Accuracy', 'F1-score', 'Precision', 'Sens.', 'Spec.') 
+
+tbl_SYNTSPEAK1<- data.frame(nsvm, acc_IF_SYNTSPEAK1, F1_IF_SYNTSPEAK1,  Precision_IF_SYNTSPEAK1, 
+                            Sens_IF_SYNTSPEAK1, Spec_IF_SYNTSPEAK1)
+names(tbl_SYNTSPEAK1)<- cnames_1
+
+ltabl_SYNTSPEAK1<-kable(tbl_SYNTSPEAK1, format = "latex", longtable = T, caption = "Results of SYNT vs SPEAK1 SVM
+                        with Instantaneous Frequencies of each IMFs as features - Kernel:RBF.")%>%
+  kable_styling(latex_options = c("striped", "hold_position"),       #stripe_color  
+                full_width = F) 
+
+rm(acc_IF_SYNTSPEAK1, F1_IF_SYNTSPEAK1, Kappa_IF_SYNTSPEAK1, Precision_IF_SYNTSPEAK1, 
+   Sens_IF_SYNTSPEAK1, Spec_IF_SYNTSPEAK1)
+
+
+
+
+
+
+
---
+++ Code/OutOfSample_Code/Laplace_Kernel/SVM_outofsample_SYNT_SPEAK1_stat_COEFF.R
@@ -0,0 +1,74 @@
+#SVM OUT OF SAMPLE - SYNT vs SPEAKER1 ---  FEATURE: stat --- KERNEL: RBASIS  
+library("ROCR")
+#library("pROC")
+library('ggplot2')
+library('lattice')
+library('caret')
+library('kernlab')
+
+m2<- 20
+y11f<- rep("uno",m2)
+y22f<- rep("zero",m2)
+yff<- as.factor(c(y11f,y22f))
+
+#-----------------------------------------------------------------------------------------------------
+x_SYNT_stat_b<- matrix(unlist(synt_vec_melt_b), byrow=TRUE, nrow=20 )
+x_SYNT_stat_c<- matrix(unlist(synt_vec_melt_c), byrow=TRUE, nrow=20 )
+x_SYNT_stat_d<- matrix(unlist(synt_vec_melt_d), byrow=TRUE, nrow=20 )
+
+x_synt_stat_coeff_new<- cbind(x_SYNT_stat_b,x_SYNT_stat_c,x_SYNT_stat_d)
+
+x_speak1_stat_b<- matrix(unlist(speak1_vec_melt_b), byrow=TRUE, nrow=20 )
+x_speak1_stat_c<- matrix(unlist(speak1_vec_melt_c), byrow=TRUE, nrow=20 )
+x_speak1_stat_d<- matrix(unlist(speak1_vec_melt_d), byrow=TRUE, nrow=20 )
+
+x_speak1_stat_coeff_new <- cbind(x_speak1_stat_b,x_speak1_stat_c,x_speak1_stat_d)
+
+x_syntspeak1_stat_coeff_new<- rbind(x_synt_stat_coeff_new, x_speak1_stat_coeff_new)
+
+colnames(x_syntspeak1_stat_coeff_new) <- paste("x_syntspeak1_stat_coeff", 1:ncol(x_syntspeak1_stat_coeff_new), sep="")
+
+x_IMF1 <- cbind(x_syntspeak1_stat_coeff_new[,1:70],x_syntspeak1_stat_coeff_new[,351:420],x_syntspeak1_stat_coeff_new[,701:770] )
+x_IMF2<- cbind(x_syntspeak1_stat_coeff_new[,71:140],x_syntspeak1_stat_coeff_new[,421:490],x_syntspeak1_stat_coeff_new[,771:840] )
+x_IMF3<- cbind(x_syntspeak1_stat_coeff_new[,141:210],x_syntspeak1_stat_coeff_new[,491:560],x_syntspeak1_stat_coeff_new[,841:910] )
+x_IMF4<- cbind(x_syntspeak1_stat_coeff_new[,211:280],x_syntspeak1_stat_coeff_new[,561:630],x_syntspeak1_stat_coeff_new[,911:980] )
+x_IMF5<- cbind(x_syntspeak1_stat_coeff_new[,281:350],x_syntspeak1_stat_coeff_new[,631:700],x_syntspeak1_stat_coeff_new[,981:1050] )
+
+
+#stat - FIRST stat
+
+ypred_syntspeak1_stat1<- predict(SVM_syntspeak1_stat1_lp,newdata = x_IMF1)
+
+CM_syntspeak1_stat1_new<- confusionMatrix(ypred_syntspeak1_stat1, yff)
+CM_syntspeak1_stat1_new
+
+#stat - SECOND stat
+
+ypred_syntspeak1_stat2<- predict(SVM_syntspeak1_stat2_lp,newdata = x_IMF2)
+
+CM_syntspeak1_stat2_new<- confusionMatrix(ypred_syntspeak1_stat2, yff)
+CM_syntspeak1_stat2_new
+
+#stat - THIRD stat
+
+ypred_syntspeak1_stat3<- predict(SVM_syntspeak1_stat3_lp,newdata = x_IMF3)
+
+CM_syntspeak1_stat3_new<- confusionMatrix(ypred_syntspeak1_stat3, yff)
+CM_syntspeak1_stat3_new
+
+
+#stat - FOURTH stat
+
+ypred_syntspeak1_stat4<- predict(SVM_syntspeak1_stat4_lp,newdata = x_IMF4)
+
+CM_syntspeak1_stat4_new<- confusionMatrix(ypred_syntspeak1_stat4, yff)
+CM_syntspeak1_stat4_new
+
+#stat - FstatTH stat
+
+ypred_syntspeak1_stat5<- predict(SVM_syntspeak1_stat5_lp,newdata = x_IMF5)
+
+CM_syntspeak1_stat5_new<- confusionMatrix(ypred_syntspeak1_stat5, yff)
+CM_syntspeak1_stat5_new
+
+
